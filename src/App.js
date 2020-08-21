e7a62794fde4348c60a600bd8d58062567f6e75d
rabobahago <rabobahago@gmail.com>
2020-08-21 14:58:29 +0100

    action ownProps added

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import CakeContainer from './components/CakeContainer';
 import CakeHookContainer from './components/CakeHookContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
+        <ItemContainer/>
+        <ItemContainer cake/>
         <CakeContainer/>
         <CakeHookContainer/>
         <IceCreamContainer/>
         <NewCakeContainer/>
+        
       </div>
     </Provider>
   );
